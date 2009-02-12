835c9c6aa63f3913a0b0309084bd56e11cd21f3b
Max Warnock <circuitshaman@gmail.com>
2009-02-12 09:52:01 -0500

    Barry Daly's Requested Changes + Search Query Exception Handling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,21 +13,27 @@ class SearchController < ApplicationController
       session[:search_string] = @search_string
       @reasons_for = params[:reason_for]
       contains_reason = @reasons_for.values.include?("1")
+      @reasons_for["Review preparatory to research"] == "1" ? session[:anonymize] = true : session[:anonymize] = false
       new_search = true
     else
       @search_string = session[:search_string]
       new_search = false
     end
 
-    @reports = IdxReport.search(@search_string, :page => @page, :per_page => 10) if contains_reason or not new_search
-    @reports ||= []
-    jsoned_reasons = (@reasons_for.keys.inject([]) {|list,key| list.push(key) if @reasons_for[key] == "1"; list }).to_json if contains_reason
-    @search_log = SearchLog.create(:username => session[:username], :search_string => @search_string, :reason_for => jsoned_reasons) if contains_reason or not new_search
+    begin
+      @reports = IdxReport.search(@search_string, :page => @page, :per_page => 10) if contains_reason or not new_search
+      @reports ||= []
+      jsoned_reasons = (@reasons_for.keys.inject([]) {|list,key| list.push(key) if @reasons_for[key] == "1"; list }).to_json if contains_reason
+      @search_log = SearchLog.create(:username => session[:username], :search_string => @search_string, :reason_for => jsoned_reasons) if contains_reason or not new_search
+    rescue Ultrasphinx::UsageError
+      search_error = $!.to_s
+      search_error.gsub!("index idxreportidx: ", "")
+    end
 
     respond_to do |format|
-      format.js { render :partial => "search/results", :locals => {:reports => @reports, :search_terms => @search_string} } if contains_reason or not new_search
-      format.js { render :text => "<br /><span class=\"error-message\">You must specify a reason for the search</span>" } if not contains_reason
-      format.html
+      format.js { render :partial => "search/results", :locals => {:reports => @reports, :search_terms => @search_string} } if not search_error and (contains_reason or not new_search)
+      format.js { render :text => "<br /><span class=\"error-message\">You must specify a reason for the search</span>" } if not search_error and not contains_reason
+      format.js { render :text => "<br /><span class=\"error-message\">#{search_error}</span>" } if search_error
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def anonymize_appropriately(&block)
+    out = capture(&block)
+    concat(out, block.binding) unless session[:anonymize]
+  end
 end
